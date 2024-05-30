695b263c14cd157bc0561e1235d903a3aae2f0b8
Mateusz Budzyński <budzynekm09@gmail.com>
2024-05-30 11:29:23 +0200

    rename card2 to other in is_meld method and special method for checking equals

--- Card.py
+++ Card.py
@@ -10,9 +10,12 @@ class Card:
         self.suit = suit
         self.value = rank_value_dict[rank]
 
+    def __eq__(self, other: 'Card') -> bool:
+        return self.rank == other.rank and self.suit == other.suit
+
     def is_part_of_meld(self) -> bool:
         return self.rank in ['Q', 'K']
 
-    def is_meld(self, card2: 'Card'):
-        return self.is_part_of_meld() and card2.is_part_of_meld() and \
-            self.rank != card2.rank and self.suit == card2.suit
+    def is_meld(self, other: 'Card'):
+        return self.is_part_of_meld() and other.is_part_of_meld() and \
+            self.rank != other.rank and self.suit == other.suit
