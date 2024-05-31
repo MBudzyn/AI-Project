ac3fd4516239f7013259206fff9c16deb39b8af3
Mateusz Budzyński <budzynekm09@gmail.com>
2024-05-31 16:05:10 +0200

    .

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,3 +1,5 @@
 RANK_VALUE_DICT = {'N': 0, 'T': 10, 'J': 2, 'Q': 3, 'K': 4, 'A': 11}
 MELD_POINTS_DICT = {'H': 100, 'D': 80, 'C': 60, 'S': 40}
 QUALITY_DICT = {"A": 0, "K": 2, "Q": 3, "J": 4, "T": 1, "N": 5}
+REVERSED_QUALITY_DICT = {0: "A", 1: "T", 2: "K", 3: "Q", 4: "J", 5: "N"}
+POINTS_IN_ROUND = [11, 10, 14, 10, 9, 16, 20, 30]
